7c24e93a6f2248d1472ad351ded7acae2680c38b
Ismael <ismaeldominici.dev@gmail>
2025-11-06 18:46:27 -0400

    tipos de datos, y primer ejercicio

---
+++ Learning/JS/data-types.js
@@ -0,0 +1,32 @@
+// String
+let isString = "text"
+
+// Number
+let isInteger = 25
+let isDecimal = 34.12
+
+// Boolean
+let isTrue = true
+let isFalse = false
+
+// Undefined
+let isUndefined
+
+// Null
+let isNull = null
+
+// Symbol
+let isSymbol = Symbol()
+
+// BigInt
+let isBigInteger = 89136128321873678436287462873n // Ends with "n" or with the built-in BigInt() function
+
+console.log(typeof(isString))
+console.log(typeof(isInteger))
+console.log(typeof(isDecimal))
+console.log(typeof(isTrue))
+console.log(typeof(isFalse))
+console.log(typeof(isUndefined))
+console.log(typeof(isNull))
+console.log(typeof(isSymbol))
+console.log(typeof(isBigInteger))
