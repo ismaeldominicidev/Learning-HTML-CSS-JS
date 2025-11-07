0d82fbc7fa042fca07f229b46497c856af08461b
Ismael <ismaeldominici.dev@gmail>
2025-11-07 17:26:28 -0400

    Aprendiendo a manejar operadores

---
+++ Learning/JS/operators.js
@@ -0,0 +1,28 @@
+let y = 5
+let x = 5
+
+console.log("Arithmetic Operators")
+console.log(y + x) // Sum
+console.log(y - x) // subtract
+console.log(y * x) // Multiplication
+console.log(y / x) // Division
+console.log(y ** x) // Exponent
+console.log(y % x) // Module
+
+y++ // Increase 
+console.log(y)
+x-- // Decrease
+console.log(x)
+
+console.log("\nAssignment Operators")
+let i = 5 // Equal to
+console.log(i += 5) // Sum and assign 
+console.log(i -= 5) // subtract and assign
+console.log(i *= 5) // multiplication  and assign
+console.log(i /= 5) // Division and assign
+console.log(i **= 5) // Exponent and assign
+console.log(i %= 5) // Module and assign
+
+console.log("\nComparison Operators")
+x = 5, y = 5
+
--- Learning/JS/practices.js
+++ Learning/JS/practices.js
@@ -1,85 +1,85 @@
 // 1. Escribe un comentario en una línea
 
-// This is a comment in one line
+    // This is a comment in one line
 
 // 2. Escribe un comentario en varias líneas
 
-/*
-This is a comment in
-more lines
-*/
+    /*
+    This is a comment in
+    more lines
+    */
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
-let firstName = "Ismael Dominici"
-let age = 25
-let isLegalAge = true
-let isUndefined
-let isNull = null
-let isSymbol = Symbol()
-let isBigInteger = 8364732574523423n
+    let firstName = "Ismael Dominici"
+    let age = 25
+    let isLegalAge = true
+    let isUndefined
+    let isNull = null
+    let isSymbol = Symbol()
+    let isBigInteger = 83647325745234232342n
 
 // 4. Imprime por consola el valor de todas las variables
 
-console.log("Value of Variables:\n")
+    console.log("Value of Variables:\n")
 
-console.log(firstName)
-console.log(age)
-console.log(isLegalAge)
-console.log(isUndefined)
-console.log(isNull)
-console.log(isSymbol)
-console.log(isBigInteger)
+    console.log(firstName)
+    console.log(age)
+    console.log(isLegalAge)
+    console.log(isUndefined)
+    console.log(isNull)
+    console.log(isSymbol)
+    console.log(isBigInteger)
 
 // 5. Imprime por consola el tipo de todas las variables
 
-console.log("\nType of Variables:\n")
+    console.log("\nType of Variables:\n")
 
-console.log(typeof(firstName))
-console.log(typeof(age))
-console.log(typeof(isLegalAge))
-console.log(typeof(isUndefined))
-console.log(typeof(isNull))
-console.log(typeof(isSymbol))
-console.log(typeof(isBigInteger))
+    console.log(typeof(firstName))
+    console.log(typeof(age))
+    console.log(typeof(isLegalAge))
+    console.log(typeof(isUndefined))
+    console.log(typeof(isNull))
+    console.log(typeof(isSymbol))
+    console.log(typeof(isBigInteger))
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
 
-firstName = "Manuel Alfredo"
-age = 15
-isLegalAge = false
-isUndefined
-isNull = null
-isSymbol = Symbol("Hola")
-isBigInteger = 234832648732483264n
+    firstName = "Manuel Alfredo"
+    age = 15
+    isLegalAge = false
+    isUndefined
+    isNull = null
+    isSymbol = Symbol("Hola")
+    isBigInteger = 234832648732483264n
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
 
-firstName = 23
-age = "Zacarias Ferreira"
-isLegalAge = 2347286348323423432473249n
-isUndefined = "definido"
-isNull = true
-isSymbol = null
-isBigInteger = 234.231
+    firstName = 23
+    age = "Zacarias Ferreira"
+    isLegalAge = 2347286348323423432473249n
+    isUndefined = "definido"
+    isNull = true
+    isSymbol = null
+    isBigInteger = 234.231
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
-const myName = "Ismael Dominici"
-const birthday = 25
-const isHuman = true
-// const god | An undefined constant cannot be declared
-const thisNull = null
-const thisSymbol = Symbol("Hello")
-const ageUniverse = 13800000000n
+    const myName = "Ismael Dominici"
+    const birthday = 25
+    const isHuman = true
+    // const god | An undefined constant cannot be declared
+    const thisNull = null
+    const thisSymbol = Symbol("Hello")
+    const ageUniverse = 13800000000n
 
 // 9. A continuación, modifica los valores de las constantes
 
-// myName = "Rafael Ortiz"
-// birthday = 30
-// isHuman = false
-// thisNull = "notNull"
-// thisSymbol = Symbol("Amen")
-// ageUniverse = 26700000000n
+    // myName = "Rafael Ortiz"
+    // birthday = 30
+    // isHuman = false
+    // thisNull = "notNull"
+    // thisSymbol = Symbol("Amen")
+    // ageUniverse = 26700000000n
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
